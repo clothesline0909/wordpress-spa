04ac188c7029e0ce68caa879cae48df15eb1f529
Brett Clark <clarkbab@gmail.com>
2016-05-17 08:49:54 +1000

    Added: Server.js (configuration for development server).

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 var Webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
-var Config = require('./r')
+var Config = require('./webpack.config');
+
+new WebpackDevServer(webpack(Config),
+	{
+		publicPath: Config.output.publicPath,
+		hot: true,
+		historyApiFallback: true,
+		stats: {
+			colors: true
+		}
+	}
+).listen(3000, 'localhost', function (error) {
+	if (error) {
+		console.log(error);
+	}
+
+	console.log('Listening at localhost:3000');
+})
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React, {component} from 'react';
+import ReactDOM from 'react-dom';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+import {Provider} from 'react-redux';
+import {Router, Route, IndexRoute} from 'react-router';
+import configureStore form './store/configureStore';
+import LexiTheme from './containers/LexiTheme';
+import ArticlePage from './containers/ArticlePage';
+import ArticleListingPage from './containers/ArticleListingPage';
+import AboutPageContainer from './containers/AboutPageContainer';
+import '../sass/bootstrap.css';
+import '../sass/bootstrap-blog.css';
+
+const history = new createBrowserHistory();
+const store = configureStore();
+let rootElement = document.getElementById('root');
+
+ReactDOM.render(
+	<Provider store={store}>
+		<Router history={history}>
+			<Route path="/" component={LexiTheme}>
+				<IndexRoute component={ArticleListingPage}/>
+				<Route path="about" component={AboutPageContainer}/>
+				<Route path=":year/:month/:name" component={ArticlePage}/>
+			</Route>
+		</Router>
+	</Provider>,
+	rootElement
+);
